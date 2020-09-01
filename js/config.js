81f048aed5e921962583148c0282a1433d483b4f
vinodpahuja <vinod.pahuja@gmail.com>
2020-09-01 11:01:54 +0000

    Added Dev config

--- js/config.js
+++ js/config.js
@@ -1,3 +1,8 @@
+// Prod
 // tenantUrl = '';
 configUrl = 'https://jsoneditoronline.herokuapp.com/v1/docs/7a82f426331541cda2d63e01a74f1e83';
-//dataUrl = '';
+// dataUrl = '';
+// sectionUrl = '';
+
+// Dev
+// configUrl = 'https://jsoneditoronline.herokuapp.com/v1/docs/aec911abd663434c9585a6cecc9b6825';
