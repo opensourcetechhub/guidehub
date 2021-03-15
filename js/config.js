2824160db61fae65251a83f1929a7eba3de27450
Vinod Pahuja <vinodpahuja@users.noreply.github.com>
2021-03-15 17:18:22 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,40 @@
-// Prod
-// tenantUrl = '';
-configUrl = 'https://jsoneditoronline.herokuapp.com/v1/docs/7a82f426331541cda2d63e01a74f1e83';
-// dataUrl = '';
-// sectionUrl = '';
+config = {
+  "logo": "../os/images/icon/logo.png",
+  "name": "OpenSourceHub",
+  "nav": "Y",
+  "filter": "N",
+  "data": "",
+  "section": ""
+};
 
-// Dev
-// configUrl = 'https://jsoneditoronline.herokuapp.com/v1/docs/aec911abd663434c9585a6cecc9b6825';
+section: [ {
+  "id" : "fw",
+  "name" : "Frameworks",
+  "state" : "active"
+}, {
+  "id" : "lib",
+  "name" : "Libraries"
+}, {
+  "id" : "tool",
+  "name" : "Tools"
+} ]
+
+data : [ {
+  "section": "fw",
+  "id": "spring",
+  "name": "Spring",
+  "desc": "",
+  "type": "preview",
+  "1cover": "https://spring.io/images/spring-logo-9146a4d3298760c2e7e49595184e1975.svg",
+  "link": "https://spring.io/guides",
+  "size": "12"
+}, {
+  "section": "fw",
+  "id": "hibernate",
+  "name": "Hibernate",
+  "desc": "",
+  "type": "preview",
+  "1cover": "https://design.jboss.org/hibernate/logo/final/hibernate_icon_whitebkg_256px.png",
+  "link": "https://hibernate.org",
+  "size": "12"
+} ]
